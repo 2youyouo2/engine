1c10a639a05914fe1cbf52f865ab30d88f6f92a1
2youyou2 <501251991@qq.com>
2019-12-17 15:26:34 +0800

    upgrade box2d.ts

--- cocos2d/core/physics/platform/CCPhysicsAABBQueryCallback.js
+++ cocos2d/core/physics/platform/CCPhysicsAABBQueryCallback.js
@@ -25,50 +25,53 @@
 
 const BodyType = require('../CCPhysicsTypes').BodyType;
 
-function PhysicsAABBQueryCallback () {
-    this._point = new b2.Vec2();
-    this._isPoint = false;
-    this._fixtures = [];
-}
-
-PhysicsAABBQueryCallback.prototype.init = function (point) {
-    if (point) {
-        this._isPoint = true;
-        this._point.x = point.x;
-        this._point.y = point.y;
-    }
-    else {
+export default class PhysicsAABBQueryCallback extends b2.QueryCallback {
+    constructor () {
+        super();
+        this._point = new b2.Vec2();
         this._isPoint = false;
+        this._fixtures = [];
     }
-    
-    this._fixtures.length = 0;
-};
 
-PhysicsAABBQueryCallback.prototype.ReportFixture = function (fixture) {
-    var body = fixture.GetBody();
-    if (body.GetType() === BodyType.Dynamic) {
-        if (this._isPoint) {
-            if (fixture.TestPoint(this._point)) {
-                this._fixtures.push(fixture);
-                // We are done, terminate the query.
-                return false;
-            }
+    init (point) {
+        if (point) {
+            this._isPoint = true;
+            this._point.x = point.x;
+            this._point.y = point.y;
         }
         else {
-            this._fixtures.push(fixture);
+            this._isPoint = false;
         }
+        
+        this._fixtures.length = 0;
     }
 
-    // True to continue the query, false to terminate the query.
-    return true;
-};
+    ReportFixture (fixture) {
+        var body = fixture.GetBody();
+        if (body.GetType() === BodyType.Dynamic) {
+            if (this._isPoint) {
+                if (fixture.TestPoint(this._point)) {
+                    this._fixtures.push(fixture);
+                    // We are done, terminate the query.
+                    return false;
+                }
+            }
+            else {
+                this._fixtures.push(fixture);
+            }
+        }
+    
+        // True to continue the query, false to terminate the query.
+        return true;
+    }
+    
+    getFixture () {
+        return this._fixtures[0];
+    }
+    
+    getFixtures () {
+        return this._fixtures;
+    }
+}
 
-PhysicsAABBQueryCallback.prototype.getFixture = function () {
-    return this._fixtures[0];
-};
-
-PhysicsAABBQueryCallback.prototype.getFixtures = function () {
-    return this._fixtures;
-};
-
-cc.PhysicsAABBQueryCallback = module.exports = PhysicsAABBQueryCallback;
+cc.PhysicsAABBQueryCallback = PhysicsAABBQueryCallback;
